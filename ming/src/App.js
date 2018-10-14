81c3074ebe13ef9e5b412000e67960c00b9401e7
mingwho <ming.hu@namshi.com>
2018-10-14 21:11:05 +0400

    use react

---
+++ ming/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo2.svg';
+import banner from './banner.svg';
+import './App.css';
+
+class App extends Component{
+  render() {
+    return (
+        <div className="Outside">
+            <div className="Border">
+            </div>
+        <div className="Banner">
+          <img src={banner} className="App-banner" alt="logo"/>
+        </div>
+        </div>
+    );
+  }
+}
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+export default App;
