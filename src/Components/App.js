cadeb1e668bd68e4c0dfe05ab2b22082ba63e046
mingwho <ming.hu@namshi.com>
2018-10-14 23:33:08 +0400

    fix

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,37 +10,3 @@ const App = () => (
 );
 
 export default App
-
-
-// import React, { Component } from 'react';
-// import logo from '../logo2.svg';
-// import banner from '../banner.svg';
-// import '../App.css';
-//
-// class App extends Component{
-//   render() {
-//     return (
-//         <div>
-//         <Header />
-//         <div className="Outside">
-//             <div className="Border">
-//             </div>
-//             <div className="Banner">
-//               <img src={banner} className="App-banner" alt="logo"/>
-//             </div>
-//             <div className="Category">
-//                 <div>PROJECTS</div>
-//                 <div>ABOUT</div>
-//                 <div>BOOKS</div>
-//                 <div>CONNECT</div>
-//             </div>
-//             <div className="Line">
-//             </div>
-//         </div>
-//             <Main />
-//         </div>
-//     );
-//   }
-// }
-//
-// export default App;
