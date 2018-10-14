1e2530ed33e0cc2d7bd99986636f2ed1ac5f031c
mingwho <ming.hu@namshi.com>
2018-10-14 23:52:28 +0400

    pls work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './Header'
-import Main from './Main'
+import Header from './components/Header'
+import Main from './components/Main'
 
 const App = () => (
     <div>
