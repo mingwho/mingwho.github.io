20503380ce7380c6ba140792938a84fbb3201279
mingwho <ming.hu@namshi.com>
2018-10-15 16:56:52 +0400

    fix underline

--- src/Header.js
+++ src/Header.js
@@ -2,26 +2,31 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import banner from './banner.svg'
 import './App.css';
-// The Header creates links that can be used to navigate
-// between routes.
-const Header = () => (
-    <header>
+import styled, {css} from 'react-emotion';
 
-                 <div className="Outside">
-                     <div className="Border">
-                     </div>
-                     <div className="Banner">
-                       <img src={banner} className="App-banner" alt="logo"/>
-                     </div>
-                     <div className="Category">
-                             <div><Link to='/'>ABOUT</Link></div>
-                             <div><Link to='/projects'>PROJECTS</Link></div>
-                             <div><Link to='/books'>BOOKS</Link></div>
-                     </div>
-                     <div className="Line">
-                     </div>
-                 </div>
-    </header>
-);
+// between routes.
+class Header extends React.Component {
+    render() {
+        return(
+<header>
+            <div className="Outside">
+                <div className="Border">
+                </div>
+                <div className="Banner">
+                    <img src={banner} className="App-banner" alt="logo"/>
+                </div>
+                <div className="Category">
+                    <div><Link to='/'>ABOUT</Link></div>
+                    <div><Link to='/projects'>PROJECTS</Link></div>
+                    <div><Link to='/books'>BOOKS</Link></div>
+                </div>
+                <div className="Line">
+                </div>
+            </div>
+</header>
+
+        );
+    }
+}
 
 export default Header
