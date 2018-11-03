ddeb2d17b9c28f1386786c2c7343c3622613958c
mingwho <ming.hu@namshi.com>
2018-11-03 18:33:56 +0400

    test

---
+++ content/posts/first-try.md
@@ -0,0 +1,9 @@
+---
+title: "First Try"
+date: 2018-11-02T21:32:35+04:00
+draft: true
+---
+
+### this is just a test
+
+TESTING how things are working
--- src/About.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const About = () => (
-    <div>
-        <h1>Welcome to the my website!</h1>
-    </div>
-);
-
-export default About
